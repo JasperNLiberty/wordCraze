e783090a750befae26d1bbc35534e7ef4b27f931
noslack <NoSlack@Lis-MacBook-Pro.local>
2018-02-07 23:50:24 -0500

    adding pods and UI

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'wordCraze' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for wordCraze
+    pod 'SwiftyJSON'
+    pod 'Alamofire'
+end
--- wordCraze/ViewController.swift
+++ wordCraze/ViewController.swift
@@ -8,59 +8,39 @@
 
 import UIKit
 import AVFoundation
+import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController {
 
     var player:AVPlayer?
     var playerItem:AVPlayerItem?
+    var wordArray: [String]?
+    
+    @IBOutlet weak var currentWordLabel: UILabel!
+    
+    
+    @IBAction func generateRandomNewWord(_ sender: Any) {
+        let randomId = Int(arc4random_uniform(UInt32(wordArray!.count)))
+        let newWord:String = wordArray![randomId]
+        currentWordLabel.text = newWord
+        readWord(word: newWord)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let appId = "e28c1363"
-        let appKey = "1b7bf2d9b08c9a6de6f1041bd2131cfe"
-        let language = "en"
-        let word = "serendipity"
-        let word_id = word.lowercased() //word id is case sensitive and lowercase is required
-        let filters = "pronunciations;regions=us"
-        let url = URL(string: "https://od-api.oxforddictionaries.com:443/api/v1/entries/\(language)/\(word_id)/\(filters)")!
-        
-        var request = URLRequest(url: url)
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.addValue(appId, forHTTPHeaderField: "app_id")
-        request.addValue(appKey, forHTTPHeaderField: "app_key")
-        
-        let session = URLSession.shared
-        _ = session.dataTask(with: request, completionHandler: { data, response, error in
-            if let response = response,
-                let data = data,
-                let jsonData = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any]
-             {
-                let results = jsonData!["results"] as! [[String: Any]]
-                let a = results[0]
-                let b = a["lexicalEntries"] as! [[String: Any]]
-                let c = b[0]
-                let d = c["pronunciations"] as! [[String: Any]]
-                let e = d[0]
-                let f = e["audioFile"] as! String
-                print(f)
-                
-                                
-                let url = URL(string: f)
-                
-                let playerItem:AVPlayerItem = AVPlayerItem(url: url!)
-                self.player = AVPlayer(playerItem: playerItem)
-                
-                self.player!.play()
-               
-                
-            } else {
-                print(error)
-                print(NSString.init(data: data!, encoding: String.Encoding.utf8.rawValue))
+        do {
+            // This solution assumes  you've got the file in your bundle
+            if let path = Bundle.main.path(forResource: "words", ofType: "txt"){
+                let data = try String(contentsOfFile:path, encoding: String.Encoding.utf8)
+                wordArray = data.components(separatedBy: "\n")
             }
-        }).resume()
-        
-        
+        } catch let err as NSError {
+            // do something with Error
+            print(err)
+        }
         
     }
 
@@ -70,6 +50,62 @@ class ViewController: UIViewController {
     }
 
 
+    
+    func readWord(word: String) {
+        let appId = "e28c1363"
+        let appKey = "1b7bf2d9b08c9a6de6f1041bd2131cfe"
+        let language = "en"
+        let word = word
+        let word_id = word.lowercased() //word id is case sensitive and lowercase is required
+        let filters = "pronunciations;regions=us"
+        let url = URL(string: "https://od-api.oxforddictionaries.com:443/api/v1/entries/\(language)/\(word_id)/\(filters)")!
+        
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue(appId, forHTTPHeaderField: "app_id")
+        request.addValue(appKey, forHTTPHeaderField: "app_key")
+        
+//        let params : [String : String] = ["app_id" : appId, "app_key": appKey]
+        
+//        Alamofire.request(url, method: .get, parameters: params).responseString {
+//            response in
+//            if response.result.isSuccess {
+//                print("Success! Got the weather data")
+//
+//                let weatherJSON : JSON = JSON(response.result.value!)
+//                print("ok")
+////                self.updateWeatherData(json: weatherJSON)
+//            }
+//            else {
+//                print("Error \(response.result.error)")
+////                self.cityLabel.text = "Connection Issues"
+//            }
+//        }
+        
+        
+        let session = URLSession.shared
+
+        _ = session.dataTask(with: request, completionHandler: { data, response, error in
+            if let response = response,
+                let data = data
+//                let jsonData = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            {
+                let f = JSON(data)
+                var g = f["results"][0]["lexicalEntries"][0]["pronunciations"][1]["audioFile"].rawString()!
+                if g == "null" {
+                    g = f["results"][0]["lexicalEntries"][0]["pronunciations"][0]["audioFile"].rawString()!
+                }
+                
+                let url = URL(string: g)
+                let playerItem:AVPlayerItem = AVPlayerItem(url: url!)
+                self.player = AVPlayer(playerItem: playerItem)
+                self.player!.play()
+            } else {
+                print(error)
+                print(NSString.init(data: data!, encoding: String.Encoding.utf8.rawValue))
+            }
+        }).resume()
+    }
 }
 
 
