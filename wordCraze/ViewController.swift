380b26d3a2278722f0c199b3343bbb587929cd6f
noslack <NoSlack@Lis-MacBook-Pro.local>
2018-02-08 22:04:28 -0500

    example sentences

--- wordCraze/ViewController.swift
+++ wordCraze/ViewController.swift
@@ -18,12 +18,15 @@ class ViewController: UIViewController {
     var wordArray: [String]?
     
     @IBOutlet weak var currentWordLabel: UILabel!
-    
+    @IBOutlet weak var sen1: UILabel!
+    @IBOutlet weak var sen2: UILabel!
+    @IBOutlet weak var sen3: UILabel!
     
     @IBAction func generateRandomNewWord(_ sender: Any) {
         let randomId = Int(arc4random_uniform(UInt32(wordArray!.count)))
         let newWord:String = wordArray![randomId]
         currentWordLabel.text = newWord
+        getSen(word: newWord)
         readWord(word: newWord)
     }
     
@@ -50,6 +53,49 @@ class ViewController: UIViewController {
     }
 
 
+    func getSen(word: String) {
+        let appId = "e28c1363"
+        let appKey = "1b7bf2d9b08c9a6de6f1041bd2131cfe"
+        let language = "en"
+        let word = word
+        let word_id = word.lowercased() //word id is case sensitive and lowercase is required
+        let url = URL(string: "https://od-api.oxforddictionaries.com:443/api/v1/entries/\(language)/\(word_id)/sentences")!
+        
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue(appId, forHTTPHeaderField: "app_id")
+        request.addValue(appKey, forHTTPHeaderField: "app_key")
+        
+        let session = URLSession.shared
+        
+        _ = session.dataTask(with: request, completionHandler: { data, response, error in
+            if let response = response,
+                let data = data
+            {
+                let f = JSON(data)
+                let s1 = f["results"][0]["lexicalEntries"][0]["sentences"][0]["text"].rawString()!
+                let s2 = f["results"][0]["lexicalEntries"][0]["sentences"][1]["text"].rawString()!
+                let s3 = f["results"][0]["lexicalEntries"][0]["sentences"][2]["text"].rawString()!
+                
+                DispatchQueue.main.async {
+                    if s1 != "null" {
+                        self.sen1.text = s1
+                    }
+                    if s2 != "null" {
+                        self.sen2.text = s2
+                    }
+                    if s3 != "null" {
+                        self.sen3.text = s3
+                    }
+                }
+                
+            } else {
+                print(error)
+                print(NSString.init(data: data!, encoding: String.Encoding.utf8.rawValue))
+            }
+        }).resume()
+        
+    }
     
     func readWord(word: String) {
         let appId = "e28c1363"
@@ -88,12 +134,11 @@ class ViewController: UIViewController {
         _ = session.dataTask(with: request, completionHandler: { data, response, error in
             if let response = response,
                 let data = data
-//                let jsonData = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers)
             {
                 let f = JSON(data)
-                var g = f["results"][0]["lexicalEntries"][0]["pronunciations"][1]["audioFile"].rawString()!
+                var g = f["results"][0]["lexicalEntries"][0]["pronunciations"][0]["audioFile"].rawString()!
                 if g == "null" {
-                    g = f["results"][0]["lexicalEntries"][0]["pronunciations"][0]["audioFile"].rawString()!
+                    g = f["results"][0]["lexicalEntries"][0]["pronunciations"][1]["audioFile"].rawString()!
                 }
                 
                 let url = URL(string: g)
