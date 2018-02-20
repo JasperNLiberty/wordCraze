c2c02b12a0d90665a542c6efba7eeda734cb7239
noslack <NoSlack@Lis-MacBook-Pro.local>
2018-02-19 20:47:57 -0500

    host mp3 files and return random word url

--- wordCraze/ViewController.swift
+++ wordCraze/ViewController.swift
@@ -23,11 +23,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var sen3: UILabel!
     
     @IBAction func generateRandomNewWord(_ sender: Any) {
-        let randomId = Int(arc4random_uniform(UInt32(wordArray!.count)))
-        let newWord:String = wordArray![randomId]
-        currentWordLabel.text = newWord
-        getSen(word: newWord)
-        readWord(word: newWord)
+//        let randomId = Int(arc4random_uniform(UInt32(wordArray!.count)))
+//        let newWord:String = wordArray![randomId]
+//        currentWordLabel.text = newWord
+//        getSen(word: newWord)
+        serveRandomWord()
     }
     
     override func viewDidLoad() {
@@ -97,59 +97,28 @@ class ViewController: UIViewController {
         
     }
     
-    func readWord(word: String) {
-        let appId = "e28c1363"
-        let appKey = "1b7bf2d9b08c9a6de6f1041bd2131cfe"
-        let language = "en"
-        let word = word
-        let word_id = word.lowercased() //word id is case sensitive and lowercase is required
-        let filters = "pronunciations;regions=us"
-        let url = URL(string: "https://od-api.oxforddictionaries.com:443/api/v1/entries/\(language)/\(word_id)/\(filters)")!
+    func serveRandomWord() {
         
-        var request = URLRequest(url: url)
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.addValue(appId, forHTTPHeaderField: "app_id")
-        request.addValue(appKey, forHTTPHeaderField: "app_key")
+        let url = URL(string: "http://0.0.0.0:7000/serveRandomWord")!
         
-//        let params : [String : String] = ["app_id" : appId, "app_key": appKey]
-        
-//        Alamofire.request(url, method: .get, parameters: params).responseString {
-//            response in
-//            if response.result.isSuccess {
-//                print("Success! Got the weather data")
-//
-//                let weatherJSON : JSON = JSON(response.result.value!)
-//                print("ok")
-////                self.updateWeatherData(json: weatherJSON)
-//            }
-//            else {
-//                print("Error \(response.result.error)")
-////                self.cityLabel.text = "Connection Issues"
-//            }
-//        }
-        
-        
-        let session = URLSession.shared
-
-        _ = session.dataTask(with: request, completionHandler: { data, response, error in
-            if let response = response,
-                let data = data
-            {
-                let f = JSON(data)
-                var g = f["results"][0]["lexicalEntries"][0]["pronunciations"][0]["audioFile"].rawString()!
-                if g == "null" {
-                    g = f["results"][0]["lexicalEntries"][0]["pronunciations"][1]["audioFile"].rawString()!
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+                    
+                    let resDictionary = response.result.value! as! NSDictionary
+                    let wordURL = resDictionary["mp3link"]!
+                    let playerItem:AVPlayerItem = AVPlayerItem(url: URL(string: wordURL as! String)!)
+                    self.player = AVPlayer(playerItem: playerItem)
+                    self.player!.play()
+                    
+                    let word = resDictionary["word"]
+                    self.currentWordLabel.text = word as! String
+                    
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
                 }
-                
-                let url = URL(string: g)
-                let playerItem:AVPlayerItem = AVPlayerItem(url: url!)
-                self.player = AVPlayer(playerItem: playerItem)
-                self.player!.play()
-            } else {
-                print(error)
-                print(NSString.init(data: data!, encoding: String.Encoding.utf8.rawValue))
-            }
-        }).resume()
+        }
+        
     }
 }
 
--- wordList/hostMp3.py
+++ wordList/hostMp3.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
+import glob
+import os
+import numpy as np
 
 app = Flask(__name__)
-   
+
+hostIP = '0.0.0.0'
+hostPort = 7000
+
+@app.route("/serveRandomWord")
+def sendList():
+    # return "123"
+    files = glob.glob(os.getcwd()+'/static/*.mp3')
+    nFiles = len(files)
+    rNum = np.random.randint(nFiles)
+    mp3file = files[rNum]
+    mp3fileBasename = os.path.basename(mp3file)
+    return jsonify({"word":mp3fileBasename[0:-4],"mp3link" : 'http://'+hostIP+':'+ str(hostPort) +'/static/'+ mp3fileBasename})
+    # return 'http://'+hostIP+':'+ str(hostPort) +'/static/'+ mp3fileBasename
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=7000)
+    app.run(host=hostIP, debug=True, port=hostPort)
